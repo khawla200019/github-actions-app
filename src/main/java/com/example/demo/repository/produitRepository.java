158281fc29faee0ed15680809e41a8776bc7ee2c
khawla200019 <zainanekhawla2000@gmail.com>
2023-02-05 17:00:21 +0100

    First commit

---
+++ src/main/java/com/example/demo/repository/produitRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.entities.Produit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface produitRepository extends JpaRepository<Produit,Long> {
+
+
+}
